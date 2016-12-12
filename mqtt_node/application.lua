381c4b9aa04a16704add7741dbcac898eb9b251b
bmwt <bmwt@ghostplanet.org>
2016-12-12 12:23:55 -0800

    Update application.lua

--- mqtt_node/application.lua
+++ mqtt_node/application.lua
@@ -1,6 +1,6 @@
 -- code blatantly cribbed from other folks work.  i just connect em up
--- Remember to connect GPIO16 (D0) and RST to enable deep sleep
--- onewire (sensor) on (D3)
+-- Remember to connect (D0) and (RST) to enable reset after deep sleep
+-- onewire on (D3)
 
 --############
 --# Settings #
