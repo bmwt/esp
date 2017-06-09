402c11aa2847e87287bd7b62f3172c57d07ec79e
Brendan White <bmwt@es.net>
2017-06-09 07:30:26 -0700

    purple_pulse

--- python/purple_pulse.py
+++ python/purple_pulse.py
@@ -4,9 +4,9 @@ import math
 blue = machine.Pin(12)
 red = machine.Pin(13)
 green = machine.Pin(14)
-pwm_red = machine.PWM(red)
-pwm_blue = machine.PWM(blue)
-pwm_green = machine.PWM(green)
+pwm_red = machine.PWM(red, freq=500)
+pwm_blue = machine.PWM(blue, freq=500)
+pwm_green = machine.PWM(green, freq=500)
 
 def purple_pulse(t):
     for i in range(20):
@@ -15,5 +15,5 @@ def purple_pulse(t):
         time.sleep_ms(t)
 
 while True:
-    purple_pulse(50)
+    purple_pulse(20)
     
