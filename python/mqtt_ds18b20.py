9abbb5ce93d6c9de1f891a0d5092523c58677bce
Brendan White <bmwt@es.net>
2017-06-10 20:07:27 -0700

    lots and lots of comments

--- python/mqtt_ds18b20.py
+++ python/mqtt_ds18b20.py
@@ -8,32 +8,49 @@ from umqtt.simple import MQTTClient
 from metadata import client, topic, server
 
 def get_temp():
+    # declare the onewire bus
     ow = onewire.OneWire(machine.Pin(0))
+    # declare the ds18b20 instanfe
     ds = ds18x20.DS18X20(ow)
+    # find the sensor and get an address
     roms = ds.scan()
+    # trigger a reading.  need to wait 750 ms before getting result
     ds.convert_temp()
     time.sleep_ms(750)
+    # grab the value from the address
     for rom in roms:
         return ds.read_temp(rom)
     
 def pub_temp(temp):
+    # make an mqtt client instance
     c = MQTTClient(client, server)
+    # keep trying to connect until we do
     while True:
         try:
+            # connect to the mqtt server
             c.connect()
-            c.publish(topic, str(temp)) #publish data MQTT broker
-            c.disconnect() #Disconnects from server
+            # publish our message
+            c.publish(topic, str(temp))
+            # disconnect
+            c.disconnect()
         except: 
+            # if we get an exception, error and try again
             print("Cant connect to MQTT server, trying again")
             time.sleep(2)
             continue
         else:
+            # if it worked we can leave our infinite loop
             break
                     
+# main loop
 while True:
+    # get the temp
     results = get_temp()
+    # convert c -> f, and make it an int
     tempf = int(1.8*results+32)
+    # print value to console
     print(results)
+    # publish mqtt message
     pub_temp(tempf) 
+    # sleep
     time.sleep(30)
-    
