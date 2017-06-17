96589ee4e9889a0160eac3c5eae23f2120cc1df2
Brendan White <bmwt@es.net>
2017-06-17 16:35:38 -0700

    nice cleanup

--- python/colors.py
+++ python/colors.py
@@ -1,79 +1,63 @@
 import machine
 import time
 
-PINS = [13,14,12] # R, G, B
-R = 13
-G = 14
-B = 12
-red = machine.PWM(machine.Pin(13), freq=300)
-green = machine.PWM(machine.Pin(14), freq=300)
-blue = machine.PWM(machine.Pin(12), freq=300)
+# initialize pins for each color
+redPWM = machine.PWM(machine.Pin(13, machine.Pin.OUT), freq=1000)
+greenPWM = machine.PWM(machine.Pin(14, machine.Pin.OUT), freq=1000)
+bluePWM = machine.PWM(machine.Pin(12, machine.Pin.OUT), freq=1000)
 
-# initialize pins to white and set freq to 0 (off)
-def initalize_pins():
-    for i in PINS:
-        pin = machine.Pin(i, machine.Pin.OUT)
-
+# function to turn 0-255 color into 0-1023 PWM value 
 def duty_translate(n):
-    # translate values from 0-255 to 0-1023
     return int((float(n) / 255) * 1023)
     
-def setPixelColor(n, red, green, blue):
-    red = machine.PWM(machine.Pin(13), freq=300, duty=duty_translate(red))
-    green = machine.PWM(machine.Pin(14), freq=300, duty=duty_translate(green))
-    blue = machine.PWM(machine.Pin(12), freq=300, duty=duty_translate(blue))
+# set pixel color according to RGB value.  n for multiple bulbs
+def setPixelColor(n, redvalue, greenvalue, bluevalue):
+    redPWM.duty(duty_translate(redvalue))
+    greenPWM.duty(duty_translate(greenvalue))
+    bluePWM.duty(duty_translate(bluevalue))
     
-def color_pulse(speed, step):
-        for dutyCycle in range(1, 101, step):
-            if red.duty is not 0:
-                red.duty(dutyCycle)
-            if green.duty:
-                green.duty(dutyCycle)
-            if blue.duty is not 0:
-                blue.duty(dutyCycle)
-            time.sleep(speed)
-        for dutyCycle in range(100, 0, -step):
-            if red.duty is not 0:
-                red.duty(dutyCycle)
-            if green.duty:
-                green.duty(dutyCycle)
-            if blue.duty is not 0:
-                blue.duty(dutyCycle)
-            time.sleep(speed)
+    
+# set bulb to a color
+def color_set(color):
+    if color == 'black':
+        setPixelColor(0, 0, 0, 0)
+    elif color == 'white':
+        setPixelColor(0, 255, 255, 255)
+    elif color == 'red':
+        setPixelColor(0, 255, 0, 0)
+    elif color == 'lime':
+        setPixelColor(0, 0, 255, 0)
+    elif color == 'blue':
+        setPixelColor(0, 0, 0, 255)
+    elif color == 'yellow':
+        setPixelColor(0, 255, 255, 0)
+    elif color == 'aqua':
+        setPixelColor(0, 0, 255, 255)
+    elif color == 'magenta':
+        setPixelColor(0, 255, 0, 255)
+    elif color == 'silver':
+        setPixelColor(0, 192, 192, 192)
+    elif color == 'grey':
+        setPixelColor(0, 128, 128, 128)
+    elif color == 'olive':
+        setPixelColor(0, 128, 128, 0)
+    elif color == 'green':
+        setPixelColor(0, 0, 128, 0)
+    elif color == 'purple':   
+        setPixelColor(0, 128, 0, 255)
+    elif color == 'teal':
+        setPixelColor(0, 0, 128, 128)
+    elif color == 'navy':
+        setPixelColor(0, 0, 0, 128)
+    elif color == 'orange':
+        setPixelColor(0, 255, 128, 0)
 
+# cycle through a list of colors
 def color_cycle(sleep):
-    setPixelColor(0, 0, 0, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 255, 255, 255)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 255, 0, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 255, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 0, 255)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 255, 255, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 255, 255)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 255, 0, 255)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 192, 192, 192)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 128, 128, 128)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 128, 128, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 128, 0)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 128, 0, 128)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 128, 128)
-    time.sleep_ms(sleep)
-    setPixelColor(0, 0, 0, 128)
-    time.sleep_ms(sleep)
-    
-initalize_pins()
+    cycle_colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+    for color in cycle_colors:
+        color_set(color)
+        time.sleep_ms(sleep)
+
 while True:
     color_cycle(500)
- 
